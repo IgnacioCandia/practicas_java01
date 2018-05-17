0f8ff72e801d573253cf4a0407f5a6d2c0810998
IgnacioCandia <38965856+IgnacioCandia@users.noreply.github.com>
2018-05-17 09:36:44 -0500

    Evidencia1

---
+++ java01.java
@@ -0,0 +1,34 @@
+public class Program{
+    public static void main(String[] args){
+        int a =5;
+        System.out.println("128"+a)
+    }
+}
+
+public class Program{
+    public static void main(String[] args){
+        short a =32767;
+        System.out.println("128"+a);
+    }
+}
+
+public class Program{
+    public static void main(String[] args){
+        long a =2147483647;
+        System.out.println("128"+a);
+    }
+}
+
+public class Program{
+    public static void main(String[] args){
+        float a =3.48f;
+        System.out.println("128"+a);
+    }
+}
+
+public class Program{
+    public static void main(String[] args){
+        double a =1.7;
+        System.out.println("128"+a);
+    }
+}
